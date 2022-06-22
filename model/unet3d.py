a1fb1a082e00ab614affa59795df4ace81016543
zhao <1934109821@qq.com>
2022-06-22 15:05:15 +0800

    update

--- model/unet3d.py
+++ model/unet3d.py
@@ -2,9 +2,26 @@ import torch
 from torch import nn
 
 
-class DoubleConv(nn.Module):
+class DoubleConvLeft(nn.Module):
     def __init__(self, in_channels, out_channels):
-        super(DoubleConv, self).__init__()
+        super(DoubleConvLeft, self).__init__()
+
+        self.double_conv = nn.Sequential(
+            nn.Conv3d(in_channels=in_channels, out_channels=out_channels//2, kernel_size=3, stride=1, padding=1),
+            nn.BatchNorm3d(num_features=out_channels//2),
+            nn.ReLU(inplace=True),
+            nn.Conv3d(in_channels=out_channels//2, out_channels=out_channels, kernel_size=3, stride=1, padding=1),
+            nn.BatchNorm3d(num_features=out_channels),
+            nn.ReLU(inplace=True)
+        )
+
+    def forward(self, x):
+        return self.double_conv(x)
+
+
+class DoubleConvRight(nn.Module):
+    def __init__(self, in_channels, out_channels):
+        super(DoubleConvRight, self).__init__()
 
         self.double_conv = nn.Sequential(
             nn.Conv3d(in_channels=in_channels, out_channels=out_channels, kernel_size=3, stride=1, padding=1),
@@ -23,21 +40,17 @@ class UNet(nn.Module):
     def __init__(self, n_channels, n_classes):
         super(UNet, self).__init__()
 
-        self.left_conv1 = DoubleConv(in_channels=n_channels, out_channels=64)
+        self.left_conv1 = DoubleConvLeft(in_channels=n_channels, out_channels=64)
         self.down = nn.MaxPool3d(kernel_size=2, stride=2)
-        self.left_conv2 = DoubleConv(in_channels=64, out_channels=128)
-        self.left_conv3 = DoubleConv(in_channels=128, out_channels=256)
-        self.left_conv4 = DoubleConv(in_channels=256, out_channels=512)
-        self.center_conv = DoubleConv(in_channels=512, out_channels=1024)
-
-        self.up1 = nn.ConvTranspose3d(in_channels=1024, out_channels=512, kernel_size=2, stride=2, padding=0)
-        self.right_conv1 = DoubleConv(in_channels=1024, out_channels=512)
-        self.up2 = nn.ConvTranspose3d(in_channels=512, out_channels=256, kernel_size=2, stride=2, padding=0)
-        self.right_conv2 = DoubleConv(in_channels=512, out_channels=256)
-        self.up3 = nn.ConvTranspose3d(in_channels=256, out_channels=128, kernel_size=2, stride=2, padding=0)
-        self.right_conv3 = DoubleConv(in_channels=256, out_channels=128)
-        self.up4 = nn.ConvTranspose3d(in_channels=128, out_channels=64, kernel_size=2, stride=2, padding=0)
-        self.right_conv4 = DoubleConv(in_channels=128, out_channels=64)
+        self.left_conv2 = DoubleConvLeft(in_channels=64, out_channels=128)
+        self.left_conv3 = DoubleConvLeft(in_channels=128, out_channels=256)
+        self.center_conv = DoubleConvLeft(in_channels=256, out_channels=512)
+        self.up1 = nn.ConvTranspose3d(in_channels=512, out_channels=256, kernel_size=2, stride=2, padding=0)
+        self.right_conv1 = DoubleConvRight(in_channels=512, out_channels=256)
+        self.up2 = nn.ConvTranspose3d(in_channels=256, out_channels=128, kernel_size=2, stride=2, padding=0)
+        self.right_conv2 = DoubleConvRight(in_channels=256, out_channels=128)
+        self.up3 = nn.ConvTranspose3d(in_channels=128, out_channels=64, kernel_size=2, stride=2, padding=0)
+        self.right_conv3 = DoubleConvRight(in_channels=128, out_channels=64)
         self.out_cov = nn.Conv3d(in_channels=64, out_channels=n_classes, kernel_size=1, stride=1, padding=0)
 
         self.out = nn.Sigmoid()
@@ -49,26 +62,20 @@ class UNet(nn.Module):
         x2_down = self.down(x2)
         x3 = self.left_conv3(x2_down)
         x3_down = self.down(x3)
-        x4 = self.left_conv4(x3_down)
-        x4_down = self.down(x4)
-        x5 = self.center_conv(x4_down)
-        x6 = self.up1(x5)
-        temp = torch.cat([x6, x4], dim=1)
-        x6 = self.right_conv1(temp)
-        x7 = self.up2(x6)
-        temp = torch.cat([x7, x3], dim=1)
-        x7 = self.right_conv2(temp)
-        x8 = self.up3(x7)
-        temp = torch.cat([x8, x2], dim=1)
-        x8 = self.right_conv3(temp)
-        x9 = self.up4(x8)
-        temp = torch.cat([x9, x1], dim=1)
-        x9 = self.right_conv4(temp)
-        x10 = self.out_cov(x9)
+        x4 = self.center_conv(x3_down)
+        x4_up = self.up1(x4)
+        temp = torch.cat([x4_up, x3], dim=1)
+        x5 = self.right_conv1(temp)
+        x5_up = self.up2(x5)
+        temp = torch.cat([x5_up, x2], dim=1)
+        x6 = self.right_conv2(temp)
+        x6_up = self.up3(x6)
+        temp = torch.cat([x6_up, x1], dim=1)
+        x7 = self.right_conv3(temp)
+        x8 = self.out_cov(x7)
+        x9 = self.out(x8)
 
-        x11 = self.out(x10)
-
-        return x11
+        return x9
 
 
 if __name__ == '__main__':
--- utils/DiceLoss.py
+++ utils/DiceLoss.py
@@ -1,80 +1,53 @@
-import torch
-from torch import nn
+import torch.nn as nn
 import torch.nn.functional as F
 
 
 class BinaryDiceLoss(nn.Module):
-    """Dice loss of binary class
-    Args:
-        smooth: A float number to smooth loss, and avoid NaN error, default: 1
-        p: Denominator value: \sum{x^p} + \sum{y^p}, default: 2
-        predict: A tensor of shape [N, *]
-        target: A tensor of shape same with predict
-        reduction: Reduction method to apply, return mean over batch if 'mean',
-            return sum if 'sum', return a tensor of shape [N,] if 'none'
-    Returns:
-        Loss tensor according to arg reduction
-    Raise:
-        Exception if unexpected reduction
-    """
-
-    def __init__(self, smooth=1, p=2, reduction='mean'):
+    def __init__(self):
         super(BinaryDiceLoss, self).__init__()
-        self.smooth = smooth
-        self.p = p
-        self.reduction = reduction
 
-    def forward(self, predict, target):
-        assert predict.shape[0] == target.shape[0], "predict & target batch size don't match"
-        predict = predict.contiguous().view(predict.shape[0], -1)
-        target = target.contiguous().view(target.shape[0], -1)
+    def forward(self, input, targets):
+        # 获取每个批次的大小 N
+        N = targets.size()[0]
+        # 平滑变量
+        smooth = 1
+        # 将宽高 reshape 到同一纬度
+        input_flat = input.view(N, -1)
+        targets_flat = targets.view(N, -1)
 
-        num = torch.sum(torch.mul(predict, target), dim=1) + self.smooth
-        den = torch.sum(predict.pow(self.p) + target.pow(self.p), dim=1) + self.smooth
-
-        loss = 1 - num / den
-
-        if self.reduction == 'mean':
-            return loss.mean()
-        elif self.reduction == 'sum':
-            return loss.sum()
-        elif self.reduction == 'none':
-            return loss
-        else:
-            raise Exception('Unexpected reduction {}'.format(self.reduction))
+        # 计算交集
+        intersection = input_flat * targets_flat
+        N_dice_eff = (2 * intersection.sum(1) + smooth) / (input_flat.sum(1) + targets_flat.sum(1) + smooth)
+        # 计算一个批次中平均每张图的损失
+        loss = 1 - N_dice_eff.sum() / N
+        return loss
 
 
 class DiceLoss(nn.Module):
-    """Dice loss, need one hot encode input
-    Args:
-        weight: An array of shape [num_classes,]
-        ignore_index: class index to ignore
-        predict: A tensor of shape [N, C, *]
-        target: A tensor of same shape with predict
-        other args pass to BinaryDiceLoss
-    Return:
-        same as BinaryDiceLoss
-    """
-
     def __init__(self, weight=None, ignore_index=None, **kwargs):
         super(DiceLoss, self).__init__()
-        self.kwargs = kwargs
         self.weight = weight
         self.ignore_index = ignore_index
+        self.kwargs = kwargs
 
-    def forward(self, predict, target):
-        assert predict.shape == target.shape, 'predict & target shape do not match'
-        dice = BinaryDiceLoss(**self.kwargs)
+    def forward(self, input, target):
+        """
+            input tesor of shape = (N, C, H, W)
+            target tensor of shape = (N, H, W)
+        """
+        assert input.shape == target.shape, "predict & target shape do not match"
+
+        binaryDiceLoss = BinaryDiceLoss()
         total_loss = 0
-        predict = F.softmax(predict, dim=1)
 
-        for i in range(target.shape[1]):
-            if i != self.ignore_index:
-                dice_loss = dice(predict[:, i], target[:, i])
-                if self.weight is not None:
-                    assert self.weight.shape[0] == target.shape[1], \
-                        'Expect weight shape [{}], get[{}]'.format(target.shape[1], self.weight.shape[0])
-                    dice_loss *= self.weights[i]
-                total_loss += dice_loss
+        # 归一化输出
+        logits = F.softmax(input, dim=1)
+        C = target.shape[1]
 
-        return total_loss / target.shape[1]
+        # 遍历 channel，得到每个类别的二分类 DiceLoss
+        for i in range(C):
+            dice_loss = binaryDiceLoss(logits[:, i], target[:, i])
+            total_loss += dice_loss
+
+        # 每个类别的平均 dice_loss
+        return total_loss / C

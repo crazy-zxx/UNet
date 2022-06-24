ee2ebdf62d464df3c674d345debdef78bb4b13c8
zhao <1934109821@qq.com>
2022-06-24 19:26:27 +0800

    update

---
+++ test.py
@@ -0,0 +1,58 @@
+import os
+from pathlib import Path
+
+import SimpleITK as sitk
+import torch
+from torch.utils.data import DataLoader
+
+from data.Hippocampus import Hippocampus, resize_image_itk
+from model.unet3d import UNet
+from utils.oneHot import onehot2mask
+
+test_datasets_path = r'./datasets/3d/hippocampus'
+model_path = r'./saved_model_3d_hippocampus/best_model.pth'
+pred_save_path = r'./pred_3d_hippocampus'
+n_channels = 1
+n_classes = 3
+device = 'cuda' if torch.cuda.is_available() else 'cpu'
+
+test_datasets = Hippocampus(dirname=test_datasets_path, train=False)
+
+
+def test():
+    test_dataloader = DataLoader(test_datasets, batch_size=1, shuffle=False, num_workers=0)
+    # load model net
+    model = UNet(n_channels=n_channels, n_classes=n_classes).to(device)
+    # load model parameters
+    model.load_state_dict(torch.load(model_path))
+
+    # predict
+    model.eval()
+    with torch.no_grad():
+        for i, img in enumerate(test_dataloader):
+            img = img.to(device)
+            pred = model(img).squeeze().cpu().numpy()
+            pred_image = onehot2mask(pred)
+            # get original info of nii
+            itk_image = sitk.ReadImage(test_datasets.images[i])
+            image_size = itk_image.GetSize()
+            image_spacing = itk_image.GetSpacing()
+            image_origin = itk_image.GetOrigin()
+            image_direction = itk_image.GetDirection()
+            # set info for predicted nii
+            pred_itk_img = sitk.GetImageFromArray(pred_image)
+            pred_itk_img.SetSpacing(image_spacing)  # 设置spacing
+            pred_itk_img.SetOrigin(image_origin)
+            pred_itk_img.SetDirection(image_direction)
+            pred_itk_img = resize_image_itk(pred_itk_img, image_size)
+            # save predicted nii
+            os.makedirs(pred_save_path, exist_ok=True)
+            # get original filename
+            pred_img_name = os.path.join(pred_save_path, f'{Path(test_datasets.images[i]).stem}')
+            # save image
+            sitk.WriteImage(pred_itk_img, pred_img_name)
+            print(f'save {pred_img_name} successfully!')
+
+
+if __name__ == '__main__':
+    test()
---
+++ train.py
@@ -0,0 +1,112 @@
+import copy
+import os
+
+import numpy as np
+import torch
+from torch import optim, save
+
+from torch.utils.data import DataLoader
+from data.Hippocampus import Hippocampus
+from model.unet3d import UNet
+from utils.loss import DiceLoss, DiceBCELoss
+from utils.drawCurve import draw
+
+train_datasets_path = r'./datasets/3d/hippocampus'
+model_save_path = r'./saved_model_3d_hippocampus'
+curve_save_path = r'./curve_3d_hippocampus'
+batch_size = 1
+n_channels = 1
+n_classes = 3
+lr = 1e-2
+epochs = 100
+device = 'cuda' if torch.cuda.is_available() else 'cpu'
+# train datasets object
+train_val_datasets = Hippocampus(dirname=train_datasets_path, train=True)
+
+
+def train_val_split(ratio):
+    """ 按照比率（ratio）将训练集分割成训练集和验证集 """
+    # load train data
+    length = len(train_val_datasets)
+    # random choice sample
+    val_index = np.random.choice(range(length), int(length * ratio), replace=False)
+    # copy object
+    train_datasets = copy.copy(train_val_datasets)
+    val_datasets = copy.copy(train_val_datasets)
+    # clear list
+    train_datasets.images, train_datasets.labels, val_datasets.images, val_datasets.labels = [], [], [], []
+    # set sample
+    for i in range(length):
+        if i in val_index:
+            val_datasets.images.append(train_val_datasets.images[i])
+            val_datasets.labels.append(train_val_datasets.labels[i])
+        else:
+            train_datasets.images.append(train_val_datasets.images[i])
+            train_datasets.labels.append(train_val_datasets.labels[i])
+    del train_val_datasets
+
+    return train_datasets, val_datasets
+
+
+def train():
+    # split train datasets to train and val by ratio
+    ratio = 0.3
+    train_datasets, val_datasets = train_val_split(ratio)
+    # dataloader
+    train_dataloader = DataLoader(train_datasets, batch_size=batch_size, shuffle=True, num_workers=0)
+    val_dataloader = DataLoader(val_datasets, batch_size=1, shuffle=True, num_workers=0)
+    # model
+    model = UNet(n_channels=n_channels, n_classes=n_classes).to(device)
+    # loss function
+    loss_func = DiceBCELoss()
+    # optimizer
+    optimizer = optim.Adam(model.parameters(), lr=lr)
+
+    print('==== start train ====')
+    # train
+    train_loss = []
+    val_acc = []
+    for epoch in range(epochs):
+        model.train()
+        total_loss = 0.
+        steps = 0
+        for img, label in train_dataloader:
+            img, label = img.to(device), label.to(device)
+            pred = model(img)
+            loss = loss_func(pred, label)
+            optimizer.zero_grad()
+            loss.backward()
+            optimizer.step()
+            total_loss += loss.detach().cpu()
+            steps += 1
+        train_avg_loss = total_loss / steps
+        print(f'epoch:{epoch + 1}/{epochs} --> train loss:{train_avg_loss}')
+        train_loss.append(train_avg_loss)
+
+        # val
+        model.eval()
+        with torch.no_grad():
+            total_acc = 0.
+            steps = 0
+            for img, label in val_dataloader:
+                img = img.to(device)
+                pred = model(img).cpu()
+                total_acc += 1 - loss_func(pred, label)
+                steps += 1
+            val_avg_acc = total_acc / steps
+            print(f'epoch:{epoch + 1}/{epochs} --> val acc:{val_avg_acc}')
+            val_acc.append(val_avg_acc)
+            # save better model
+            if val_avg_acc >= max(val_acc):
+                os.makedirs(model_save_path, exist_ok=True)
+                model_name = os.path.join(model_save_path, 'best_model.pth')
+                save(model.state_dict(), model_name)
+                print(f'{model_name} , save best model successfully!')
+
+        # draw and save: loss and accuracy curve image
+        draw(epoch + 1, [train_loss, val_acc], ['train_loss', 'val_acc'], 'train-val', 'epoch', 'value',
+             ['red', 'green'], curve_save_path)
+
+
+if __name__ == '__main__':
+    train()
